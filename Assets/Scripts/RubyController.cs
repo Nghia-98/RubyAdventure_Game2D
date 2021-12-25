6a70b653a9b48d18ddb2a19589122932814293a4
Nghia-98 <hoangnghia.binhthuan@gmail.com>
2021-12-25 15:49:09 +0700

    done with video 1

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,24 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class RubyController : MonoBehaviour
-{
-  // Start is called before the first frame update
-  void Start()
-  {
+public class RubyController : MonoBehaviour {
+  private Rigidbody2D rb2d;
+  // Move Speed
+  public float speed = 2.5f;
 
+  //Health
+  public int maxHealth = 10;
+  int currentHealth;
+  public int health {
+    get { return currentHealth; }
+    set { currentHealth = value; }
+  }
+
+  // Timer for Damege
+  float timeInvincible = 2.0f;
+  bool isInvincible;
+  float invincibleTimer;
+
+
+  void Start() {
+    //Get Rigidbody2D component
+    rb2d = GetComponent<Rigidbody2D>();
+
+    // Setup Health
+    currentHealth = maxHealth;
   }
 
   // Update is called once per frame
-  void Update()
-  {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+  void Update() {
+    if (isInvincible) {
+      invincibleTimer -= Time.deltaTime;
+      if (invincibleTimer <= 0) {
+        isInvincible = false;
+      }
+    }
+  }
 
-        Vector2 position = transform.position;
-        position.x += 5 * horizontal * Time.deltaTime;
-        position.y += 5 * vertical * Time.deltaTime;
+  void FixedUpdate() {
+    float horizontal = Input.GetAxis("Horizontal");
+    float vertical = Input.GetAxis("Vertical");
 
-        transform.position = position;
+    Vector2 position = transform.position;
+    position.x += speed * horizontal * Time.deltaTime;
+    position.y += speed * vertical * Time.deltaTime;
+
+    rb2d.MovePosition(position);
+  }
+
+  public void ChangeHealth(int amount) {
+    if (amount < 0) {
+      if (isInvincible) {
+        return;
+      }
+      isInvincible = true;
+      invincibleTimer = timeInvincible;
+    }
+    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
   }
 }
